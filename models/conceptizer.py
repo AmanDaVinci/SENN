d963eb7e6b8f128d5db3ea12451c135decbc7e0d
Ivan Bardarov <iebardarov@gmail.com>
2020-01-14 12:24:59 +0100

    Handle list values and int values for CNN parameters.

--- models/conceptizer.py
+++ models/conceptizer.py
@@ -5,6 +5,7 @@ import torchvision.utils as vutils
 
 import numpy as np
 
+
 class Conceptizer(nn.Module):
     def __init__(self):
         """
@@ -39,7 +40,8 @@ class Conceptizer(nn.Module):
 
 class Conceptizer_CNN(Conceptizer):
     def __init__(self, image_size, concept_num, concept_dim, image_channels=1, encoder_channels=[10],
-                 decoder_channels=[16, 8], kernel_size=5, stride_conv=1, stride_pool=2, stride_upsample=2, padding=0):
+                 decoder_channels=[16, 8], kernel_size_conv=5, kernel_size_upsample=[5, 5, 2],
+                 stride_conv=1, stride_pool=2, stride_upsample=[2, 1, 2], padding_conv=0, padding_upsample=[0, 0, 1]):
         """
         CNN Autoencoder used to learn the concepts, present in an input image
 
@@ -49,7 +51,7 @@ class Conceptizer_CNN(Conceptizer):
             the width of the input image
         concept_num : int
             the number of concepts
-        concept_dim :
+        concept_dim : int
             the dimension of each concept to be learned
         image_channels : int
             the number of channels of the input images
@@ -57,25 +59,38 @@ class Conceptizer_CNN(Conceptizer):
             a list with the number of channels for the hidden convolutional layers
         decoder_channels : list[int]
             a list with the number of channels for the hidden upsampling layers
-        kernel_size : int
-            the size of the kernels to be used for convolution and upsampling
-        stride_conv : int
+        kernel_size_conv : int, list[int]
+            the size of the kernels to be used for convolution
+        kernel_size_upsample : int, list[int]
+            the size of the kernels to be used for upsampling
+        stride_conv : int, list[int]
             the stride of the convolutional layers
-        stride_pool : int
+        stride_pool : int, list[int]
             the stride of the pooling layers
-        stride_upsample : int
+        stride_upsample : int, list[int]
             the stride of the upsampling layers
-        padding : int
-            the padding to be used by the convolutional and upsampling layers
+        padding_conv : int, list[int]
+            the padding to be used by the convolutional layers
+        padding_upsample : int, list[int]
+            the padding to be used by the upsampling layers
         """
         super(Conceptizer_CNN, self).__init__()
         self.concept_num = concept_num
         self.dout = image_size
 
+        # Encoder params
         encoder_channels.insert(0, image_channels)
+        kernel_size_conv = handle_integer_input(kernel_size_conv, len(encoder_channels))
+        stride_conv = handle_integer_input(stride_conv, len(encoder_channels))
+        stride_pool = handle_integer_input(stride_pool, len(encoder_channels))
+        padding_conv = handle_integer_input(padding_conv, len(encoder_channels))
         encoder_channels.append(concept_num)
 
+        # Decoder params
         decoder_channels.insert(0, concept_num)
+        kernel_size_upsample = handle_integer_input(kernel_size_upsample, len(decoder_channels))
+        stride_upsample = handle_integer_input(stride_upsample, len(decoder_channels))
+        padding_upsample = handle_integer_input(padding_upsample, len(decoder_channels))
         decoder_channels.append(image_channels)
 
         # Encoder implementation
@@ -83,11 +98,12 @@ class Conceptizer_CNN(Conceptizer):
         for i in range(len(encoder_channels) - 1):
             self.encoder.append(self.conv_block(in_channels=encoder_channels[i],
                                                 out_channels=encoder_channels[i + 1],
-                                                kernel_size=kernel_size,
-                                                stride_conv=stride_conv,
-                                                stride_pool=stride_pool,
-                                                padding=padding))
-            self.dout = (self.dout - kernel_size + 2 * padding + stride_conv * stride_pool) // (stride_conv * stride_pool)
+                                                kernel_size=kernel_size_conv[i],
+                                                stride_conv=stride_conv[i],
+                                                stride_pool=stride_pool[i],
+                                                padding=padding_conv[i]))
+            self.dout = (self.dout - kernel_size_conv[i] + 2 * padding_conv[i] + stride_conv[i] * stride_pool[i]) // (
+                    stride_conv[i] * stride_pool[i])
 
         self.encoder.append(Flatten())
         self.encoder.append(nn.Linear(self.dout ** 2, concept_dim))
@@ -95,15 +111,16 @@ class Conceptizer_CNN(Conceptizer):
         # Decoder implementation
         self.unlinear = nn.Linear(concept_dim, self.dout ** 2)
         self.decoder = nn.ModuleList()
+        decoder = []
         for i in range(len(decoder_channels) - 1):
-            # the last activation needs to be Tanh
-            activation = nn.ReLU(inplace=True) if i != (len(decoder_channels) - 2) else nn.Tanh()
-            self.decoder.append(self.upsample_block(in_channels=decoder_channels[i],
-                                                    out_channels=decoder_channels[i + 1],
-                                                    kernel_size=kernel_size,
-                                                    stride_conv=stride_upsample if i % 2 == 0 else 1,
-                                                    padding=padding,
-                                                    activation=activation))
+            decoder.append(self.upsample_block(in_channels=decoder_channels[i],
+                                               out_channels=decoder_channels[i + 1],
+                                               kernel_size=kernel_size_upsample[i],
+                                               stride_deconv=stride_upsample[i],
+                                               padding=padding_upsample[i]))
+        decoder.pop()
+        decoder.append(nn.Tanh())
+        self.decoder = nn.ModuleList(decoder)
 
     def encode(self, x):
         """
@@ -183,7 +200,7 @@ class Conceptizer_CNN(Conceptizer):
             nn.ReLU(inplace=True)
         )
 
-    def upsample_block(self, in_channels, out_channels, kernel_size, stride_conv, padding, activation=nn.ReLU(inplace=True)):
+    def upsample_block(self, in_channels, out_channels, kernel_size, stride_deconv, padding):
         """
         A helper function that constructs an upsampling block with activations
 
@@ -195,12 +212,10 @@ class Conceptizer_CNN(Conceptizer):
             the number of output channels
         kernel_size : int
             the size of the convolutional kernel
-        stride_conv : int
+        stride_deconv : int
             the stride of the deconvolution
         padding : int
             the size of padding
-        activation : nn.Module
-            the activation to be used
 
         Returns
         -------
@@ -211,9 +226,9 @@ class Conceptizer_CNN(Conceptizer):
             nn.ConvTranspose2d(in_channels=in_channels,
                                out_channels=out_channels,
                                kernel_size=kernel_size,
-                               stride=stride_conv,
+                               stride=stride_deconv,
                                padding=padding),
-            activation
+            nn.ReLU(inplace=True)
         )
 
 
@@ -233,3 +248,30 @@ class Flatten(nn.Module):
             Flattened input (dim1, dim2, dim3)
         """
         return input.view(input.size(0), input.size(1), -1)
+
+
+def handle_integer_input(input, desired_len):
+    """
+    Checks if the input is an integer or a list.
+    If an integer, it is replicated the number of  desired times
+    If a list, the list is returned as it is
+
+    Parameters
+    ----------
+    input : int, list
+        The input can be either a list of parameters or a single parameter to be replicated
+    desired_len : int
+        The length of the desired list
+
+    Returns
+    -------
+    input : list[int]
+        a list of parameters which has the proper length.
+    """
+    if type(input) is int:
+        return [input] * desired_len
+    elif len(input) != desired_len:
+        raise AssertionError("The sizes of the parameters for the CNN conceptizer do not match."
+                             f"Expected {desired_len} but got {len(input)}")
+    else:
+        return input
