768532200d575a0fe673635931dac8165e8b4d28
amandavinci <amanthevinci@gmail.com>
2020-01-19 00:35:16 +0100

    add mnist robustness loss logic

--- losses.py
+++ losses.py
@@ -26,11 +26,6 @@ def compas_robustness_loss(x, aggregates, concepts, relevances):
     robustness_loss  : torch.tensor
         Robustness loss as frobenius norm of (batch_size x num_classes x num_features)
     """
-    # add class dimension
-    # TODO: this should be fixed upstream, not here
-    aggregates = aggregates.unsqueeze(-1)
-    
-    x.requires_grad_(True)
     batch_size = x.size(0)
     num_features = x.size(1)
     num_classes = aggregates.size(1)
@@ -71,27 +66,42 @@ def mnist_robustness_loss(x, aggregates, concepts, relevances):
     robustness_loss  : torch.tensor
         Robustness loss as frobenius norm of (batch_size x num_classes x num_features)
     """
-    # # add class dimension
-    # # TODO: this should be fixed upstream, not here
-    # aggregates = aggregates.unsqueeze(-1)
-    
-    # x.requires_grad_(True)
-    # batch_size = x.size(0)
-    # num_features = x.size(1)
-    # num_classes = aggregates.size(1)
+    # concept_dim is always 1
+    concepts = concepts.squeeze(-1)
+    aggregates = aggregates.squeeze(-1)
 
-    # grad_tensor = torch.ones(batch_size, num_classes) 
-    # grad_tensor.to(x.device)
-    # J_yx = torch.autograd.grad(outputs=aggregates, inputs=x, \
-    #  grad_outputs=grad_tensor, create_graph=True, only_inputs=True)[0]
-    # # bs x num_features -> bs x num_features x num_classes
-    # J_yx = J_yx.unsqueeze(-1) 
+    batch_size = x.size(0)
+    num_concepts = concepts.size(1)
+    num_classes = aggregates.size(1)
 
-    # # J_hx = Identity Matrix; h(x) is identity function
-    # robustness_loss = J_yx - relevances
+    # Jacobian of aggregates wrt x
+    jacobians = []
+    for i in range(num_classes):    
+        grad_tensor = torch.zeros(batch_size, num_classes).to(x.device)
+        grad_tensor[:,i] = 1.
+        j_yx = torch.autograd.grad(outputs=aggregates, inputs=x, \
+        grad_outputs=grad_tensor, create_graph=True, only_inputs=True)[0]
+        # bs x 1 x 28 x 28 -> bs x 784 x 1
+        jacobians.append(j_yx.view(batch_size,-1).unsqueeze(-1))
+    # bs x num_features x num_classes (bs x 784 x 10)
+    J_yx = torch.cat(jacobians, dim=2)
 
-    # return robustness_loss.norm(p='fro')
-    return torch.tensor(0.0)
+    # Jacobian of concepts wrt x
+    jacobians = []
+    for i in range(num_concepts):    
+        grad_tensor = torch.zeros(batch_size, num_concepts).to(x.device)
+        grad_tensor[:,i] = 1.
+        j_hx = torch.autograd.grad(outputs=concepts, inputs=x, \
+        grad_outputs=grad_tensor, create_graph=True, only_inputs=True)[0]
+        # bs x 1 x 28 x 28 -> bs x 784 x 1
+        jacobians.append(j_hx.view(batch_size,-1).unsqueeze(-1))
+    # bs x num_features x num_concepts
+    J_hx = torch.cat(jacobians, dim=2)
+
+    # bs x num_features x num_classes
+    robustness_loss = J_yx - torch.bmm(J_hx, relevances)
+
+    return robustness_loss.norm(p='fro')
 
 def weighted_mse(x, x_hat, sparsity):
     return sparsity * F.mse_loss(x,x_hat)
