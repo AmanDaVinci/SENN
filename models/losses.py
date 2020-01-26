34e3c32a63a8a2ae796c1df3e464d6bf1d048873
ChristophHoenes <christoph.hoenes@gmail.com>
2020-01-26 20:02:29 +0100

    fix input order sparsity loss

--- models/losses.py
+++ models/losses.py
@@ -103,13 +103,13 @@ def mnist_robustness_loss(x, aggregates, concepts, relevances):
     return robustness_loss.norm(p='fro')
 
 def weighted_mse(x, x_hat, sparsity_reg):
-    return sparsity_reg * F.mse_loss(x,x_hat)
+    return sparsity_reg * F.mse_loss(x_hat,x)
 
 def mse_kl_sparsity(x, x_hat, sparsity_reg, concepts):
-    return F.mse_loss(x,x_hat) + F.kl_div(sparsity_reg*torch.ones_like(concepts), concepts)
+    return F.mse_loss(x_hat,x) + F.kl_div(sparsity_reg*torch.ones_like(concepts), concepts)
 
 def mse_l1_sparsity(x, x_hat, sparsity_reg, concepts):
-    return F.mse_loss(x,x_hat) + sparsity_reg * torch.abs(concepts).sum()
+    return F.mse_loss(x_hat,x) + sparsity_reg * torch.abs(concepts).sum()
 
 def robustness_loss(x, parameters, model):
     return torch.tensor(0)
