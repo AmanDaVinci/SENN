19c8676f1a4eef6b50b788a32a296fa6b849f487
amandavinci <amanthevinci@gmail.com>
2020-01-27 20:44:31 +0100

    add DiSENN trainer

--- trainer.py
+++ trainer.py
@@ -46,7 +46,10 @@ def init_trainer(config_file, best_model=False):
     pprint(config)
     config = SimpleNamespace(**config)
     # create the trainer class and init with config
-    trainer = Trainer(config)
+    if hasattr(config, 'model_class') and config.model_class == 'DiSENN':
+        trainer = DiSENNTrainer(config)
+    else:
+        trainer = Trainer(config)
     return trainer
 
 
@@ -86,17 +89,11 @@ class Trainer():
 
         # Define losses
         self.classification_loss = F.nll_loss
-        self.concept_loss = eval(config.concept_loss)
+        self.concept_loss = mse_l1_sparsity
         self.robustness_loss = eval(config.robustness_loss)
 
-        # Pretrain Conceptizer if required
-        if config.pretrain_epochs > 0:
-            print("Pre-training the Conceptizer... ")
-            conceptizer = self.pretrain(conceptizer)
-
         # Init model
-        ModelClass = eval(config.model_class)
-        self.model = ModelClass(conceptizer, parameterizer, aggregator)
+        self.model = SENN(conceptizer, parameterizer, aggregator)
         self.model.to(config.device)
         self.summarize()
 
@@ -160,24 +157,12 @@ class Trainer():
             x.requires_grad_(True)
 
             # run x through SENN
-            y_pred, explanantions, x_reconstructed = self.model(x)
-
-            # visualize SENN computation graph
-            self.writer.add_graph(self.model, x)
+            y_pred, (concepts, relevances), x_reconstructed = self.model(x)
             
-            concepts, relevances = explanantions
-            if len(concepts) == 2: # DiSENN variational inference
-                concept_mean, concept_logvar = concepts
-                concepts = concept_mean
-                concept_loss = self.concept_loss(x, x_reconstructed,
-                                                 concept_mean, concept_logvar,
-                                                 self.config.beta)
-            else: # vanilla SENN
-                concept_loss = self.concept_loss(x, x_reconstructed, concepts,
-                                                 self.config.sparsity_reg)
-
             classification_loss = self.classification_loss(y_pred.squeeze(-1), labels)
             robustness_loss = self.robustness_loss(x, y_pred, concepts, relevances)
+            concept_loss = self.concept_loss(x, x_reconstructed, concepts, self.config.sparsity_reg)
+
 
             total_loss = classification_loss + \
                          self.config.robust_reg * robustness_loss + \
@@ -227,22 +212,12 @@ class Trainer():
                 labels = labels.long().to(self.config.device)
 
                 # run x through SENN
-                y_pred, explanantions, x_reconstructed = self.model(x)
-                
-                concepts, relevances = explanantions
-                if len(concepts) == 2: # DiSENN variational inference
-                    concept_mean, concept_logvar = concepts
-                    concepts = concept_mean
-                    concept_loss = self.concept_loss(x, x_reconstructed,
-                                                        concept_mean, concept_logvar,
-                                                        self.config.beta)
-                else: # vanilla SENN
-                    concept_loss = self.concept_loss(x, x_reconstructed, concepts,
-                                                     self.config.sparsity_reg)
+                y_pred, (concepts, relevances), x_reconstructed = self.model(x)
 
                 classification_loss = self.classification_loss(y_pred.squeeze(-1), labels)
                 # robustness_loss = self.robustness_loss(x, y_pred, concepts, relevances)
                 robustness_loss = torch.tensor(0.0) # jacobian cannot be computed with no_grad enabled
+                concept_loss = self.concept_loss(x, x_reconstructed, concepts, self.config.sparsity_reg)
                 
                 total_loss = classification_loss + \
                              self.config.robust_reg * robustness_loss + \
@@ -429,6 +404,41 @@ class Trainer():
         train_params = sum(p.numel() for p in self.model.parameters())
         print(f"Trainable Parameters: {train_params}\n")
 
+
+class DiSENNTrainer(Trainer):
+    """Extends general Trainer to train a DiSENN model"""
+
+    def __init__(self, config):
+        super().__init__(config)
+
+        print("Reinstantiating for DiSENN ...")
+        # get appropriate models from global namespace and instantiate them
+        try:
+            conceptizer = eval(config.conceptizer)(**config.__dict__)
+            parameterizer = eval(config.parameterizer)(**config.__dict__)
+            aggregator = eval(config.aggregator)(**config.__dict__)
+        except:
+            print("Please make sure you specify the correct Conceptizer, Parameterizer and Aggregator classes")
+            exit(-1)
+
+        # Define losses
+        self.classification_loss = F.nll_loss
+        self.concept_loss = eval(config.concept_loss)
+        self.robustness_loss = eval(config.robustness_loss)
+
+        # Init model
+        self.model = DiSENN(conceptizer, parameterizer, aggregator)
+        self.model.to(config.device)
+        self.summarize()
+
+        # Init optimizer
+        self.opt = opt.Adam(self.model.parameters())
+
+        # Pretrain Conceptizer if required
+        if self.config.pretrain_epochs > 0:
+            print("Pre-training the Conceptizer... ")
+            self.model.vae_conceptizer = self.pretrain(self.model.vae_conceptizer)
+
     def pretrain(self, conceptizer):
         """Pre-trains conceptizer on the training data to optimize the concept loss"""
 
@@ -451,3 +461,149 @@ class Trainer():
                           f"Concept Loss: {concept_loss:.3f}")
 
         return conceptizer
+
+    def train_one_epoch(self, epoch):
+        """Run one epoch of training.
+
+        Parameters
+        ----------
+        epoch : int
+            Current epoch
+        """
+        self.model.train()
+
+        for i, (x, labels) in enumerate(self.train_loader):
+            x = x.float().to(self.config.device)
+            labels = labels.long().to(self.config.device)
+            self.opt.zero_grad()
+
+            # track all operations on x for jacobian calculation
+            x.requires_grad_(True)
+            y_pred, (concepts_dist, relevances), x_reconstructed = self.model(x)
+            
+            concept_mean, concept_logvar = concepts_dist
+            concepts = concept_mean
+
+            classification_loss = self.classification_loss(y_pred.squeeze(-1), labels)
+            robustness_loss = self.robustness_loss(x, y_pred, concepts, relevances)
+            concept_loss = self.concept_loss(x, x_reconstructed,
+                                             concept_mean, concept_logvar,
+                                             self.config.beta)
+
+            total_loss = classification_loss + \
+                         self.config.robust_reg * robustness_loss + \
+                         self.config.concept_reg * concept_loss
+            total_loss.backward()
+            self.opt.step()
+
+            accuracy = self.accuracy(y_pred, labels)
+
+            # --- Report Training Progress --- #
+            self.current_iter += 1
+
+            self.writer.add_scalar('Loss/Train/Classification', classification_loss, self.current_iter)
+            self.writer.add_scalar('Loss/Train/Robustness', robustness_loss, self.current_iter)
+            self.writer.add_scalar('Loss/Train/Concept', concept_loss, self.current_iter)
+            self.writer.add_scalar('Loss/Train/Total', total_loss, self.current_iter)
+            self.writer.add_scalar('Accuracy/Train', accuracy, self.current_iter)
+
+            if i % self.config.print_freq == 0:
+                print(f"EPOCH:{epoch} STEP:{i}")
+                self.print_n_save_metrics(filename="accuracies_losses_train.csv",
+                                          total_loss=total_loss.item(),
+                                          classification_loss=classification_loss.item(),
+                                          robustness_loss=robustness_loss.item(),
+                                          concept_loss=concept_loss.item(),
+                                          accuracy=accuracy)
+
+            if self.current_iter % self.config.eval_freq == 0:
+                self.validate()
+
+    def validate(self):
+        """Validate model performance.
+
+        Model performance is validated by computing loss and accuracy measures, storing them,
+        and reporting them.
+        """
+        losses_val = []
+        classification_losses_val = []
+        concept_losses_val = []
+        robustness_losses_val = []
+        accuracies_val = []
+
+        self.model.eval()
+        with torch.no_grad():
+            for i, (x, labels) in enumerate(self.val_loader):
+                x = x.float().to(self.config.device)
+                labels = labels.long().to(self.config.device)
+
+                y_pred, (concepts_dist, relevances), x_reconstructed = self.model(x)                
+                concept_mean, concept_logvar = concepts_dist
+                concepts = concept_mean
+
+                classification_loss = self.classification_loss(y_pred.squeeze(-1), labels)
+                # robustness_loss = self.robustness_loss(x, y_pred, concepts, relevances)
+                robustness_loss = torch.tensor(0.0) # jacobian cannot be computed with no_grad enabled
+                concept_loss = self.concept_loss(x, x_reconstructed,
+                                                concept_mean, concept_logvar,
+                                                self.config.beta)
+
+                total_loss = classification_loss + \
+                             self.config.robust_reg * robustness_loss + \
+                             self.config.concept_reg * concept_loss
+
+                accuracy = self.accuracy(y_pred, labels)
+
+                losses_val.append(total_loss.item())
+                classification_losses_val.append(classification_loss.item())
+                concept_losses_val.append(concept_loss.item())
+                robustness_losses_val.append(robustness_loss.item())
+                accuracies_val.append(accuracy)
+
+            classification_loss = np.mean(classification_losses_val)
+            robustness_loss = np.mean(robustness_losses_val)
+            concept_loss = np.mean(concept_losses_val)
+            total_loss = np.mean(losses_val)
+            accuracy = np.mean(accuracies_val)
+
+            # --- Report Training Progress --- #
+            self.writer.add_scalar('Loss/Valid/Classification', classification_loss, self.current_iter)
+            self.writer.add_scalar('Loss/Valid/Robustness', robustness_loss, self.current_iter)
+            self.writer.add_scalar('Loss/Valid/Concept', concept_loss, self.current_iter)
+            self.writer.add_scalar('Loss/Valid/Total', total_loss, self.current_iter)
+            self.writer.add_scalar('Accuracy/Valid', accuracy, self.current_iter)
+
+            # --- Report Validation --- #
+            print("\n\033[93m-------- Validation --------")
+            self.print_n_save_metrics(filename="accuracies_losses_valid.csv",
+                                      total_loss=total_loss,
+                                      classification_loss=classification_loss,
+                                      robustness_loss=robustness_loss,
+                                      concept_loss=concept_loss,
+                                      accuracy=accuracy)
+            print("----------------------------\033[0m")
+            
+            if accuracy > self.best_accuracy:
+                print("\033[92mCongratulations! Saving a new best model...\033[00m")
+                self.best_accuracy = accuracy
+                self.save_checkpoint(BEST_MODEL_FILENAME)
+            else:
+                print("Saving model ...")
+                self.save_checkpoint()
+
+    def visualize(self, fname, num=3):
+        """Generates some plots to visualize the explanations.
+
+        Parameters
+        ----------
+        save_dir : str
+            Directory where the figures are saved
+        """
+        self.model.eval()
+
+        # select test example
+        (test_batch, test_labels) = next(iter(self.test_loader))
+        for i in range(num):
+            file_path = Path("results")
+            file_name = file_path / self.config.exp / "explanations.png"
+            model.explain(test_batch[i], save_as=file_name)
